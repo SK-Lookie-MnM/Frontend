0f675d1edcbaab8ba2ad5b6e02e1419dd069e14c
sunyoung <kirby@kirbyui-noteubug.local>
2024-10-06 17:40:02 +0900

    Fix: app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Mainpage from "./components/mainpage/mainpage";
-import { Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
   return (
     <div className="App">
-        <Routes>
-            <Route path="/singUp" element={<SignUp />} />
-            <Route path="/login" />
-        </Routes>
+        <div class ="black-nav">
+            <div>MNM</div>
+        </div>
         <Mainpage />
     </div>
   );
 }
 
 export default App;
+
 
