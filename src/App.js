d449743e2cb9ecb206a5c220ecf56dd7a23c3153
sunyoung <kirby@kirbyui-noteubug.local>
2024-10-07 21:45:00 +0900

    Feat: Main Page 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp/SignUp";
+import MainPage from "./pages/mainpage/mainpage";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/singUp" element={<SignUp />} />
-        <Route path="/login" />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/signUp" element={<SignUp />} />
+                <Route path="/login" />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
