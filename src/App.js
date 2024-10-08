e18c99e8537035b3b4c52cbe664029e869feec33
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-10-08 16:04:33 +0900

    feat: 네비게이션바 드롭다운 완료 (+폰트적용)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp/SignUp";
 import MainPage from "./pages/mainpage/mainpage";
+import Navbar from "./components/common/Navbar";
 
 function App() {
     return (
         <div className="App">
+            <Navbar />  {/* 모든 페이지에서 표시될 수 있게 외부에 렌더링 */}
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/signUp" element={<SignUp />} />
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,5 +1,56 @@
+// eslint-disable-next-line no-unused-vars
+import React, {useState} from "react";
+import "./Navbar.css";
+
 const Navbar = () => {
-  return <div></div>;
+  const menuLst = ["소개", "가이드", "연합", "NEWS", "캠코더 맵", "문의"];
+  const submenuLst = [
+      ["가치관", "BM • Campaign"],
+      ["사용방법"],
+      ["동맹연합"],
+      ["기사"],
+      ["학교별 맵"],
+      ["FAQ"]
+  ];
+
+  const [hide, setHide] = useState({
+      0: false,
+      1: false,
+      2: false,
+      3: false,
+      4: false,
+      5: false,
+  });
+
+  const mouseEvent = (index, bool) => {
+    const change = { ...hide };
+    change[index] = bool;
+    setHide(change);
+  };
+
+  return (
+      <nav className="nav">
+    <ul className="navContainer">
+      {menuLst.map((menu, index) => (
+          <li
+              key={index}
+              className="navItem"
+              onMouseEnter={() => mouseEvent(index, true)}
+              onMouseLeave={() => mouseEvent(index, false)}
+          >
+            <p>{menu}</p>
+            {hide[index] && (
+                <ul className="dropdown">
+                  {submenuLst[index].map((submenu, subIndex) => (
+                      <li key={subIndex}>{submenu}</li>
+                  ))}
+                </ul>
+            )}
+          </li>
+      ))}
+    </ul>
+  </nav>
+);
 };
 
 export default Navbar;
