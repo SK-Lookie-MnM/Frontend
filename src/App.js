a362b02e2f7a7f4a19f90b4d2fb6974137df982e
sunyoung <kirby@kirbyui-noteubug.local>
2024-10-12 00:00:35 +0900

    Resolved merge conflict in App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp/SignUp";
 import MainPage from "./pages/mainpage/mainpage";
 import Navbar from "./components/common/Navbar";
+import Value from './pages/Value/Value';
+import Footer from "./components/common/Footer";
+import UsageInfo from "./pages/UsageInfo/UsageInfo";
 
 function App() {
     return (
         <div className="App">
-            <Navbar />  {/* 모든 페이지에서 표시될 수 있게 외부에 렌더링 */}
+            <Navbar />
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/signUp" element={<SignUp />} />
                 <Route path="/login" />
+                <Route path="/value" element={<Value />} />
+                <Route path="/usageInfo" element={<UsageInfo />} />
             </Routes>
+            <Footer />
         </div>
     );
 }
