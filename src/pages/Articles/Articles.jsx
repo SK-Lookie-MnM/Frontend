5199e3409b7a910547fe2ac7985e6001db6c7f4d
Gongkirby <9040__@naver.com>
2024-11-07 23:49:41 +0900

    [FIX] 기사 내용 수정

--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,5 +1,5 @@
 import './articles.css';
-import example_img from '../../assets/articles/example_image.png';
+import example_img from '../../assets/articles/articles_image.jpeg';
 
 const Articles = () => {
     return (
@@ -12,13 +12,18 @@ const Articles = () => {
                 <img src={example_img} alt="예시 이미지" className="article-image"/>
                 <div className="article-content">
                     <h3 className="article-title">언론보도</h3>
-                    <h2 className="article-headline">동덕여대 재학생들의 인 캠퍼스<br/>리필스테이션 프로젝트, 성왕리에 마무리</h2>
-                    <p className="article-date">2024.09.25</p>
+                    <h2 className="article-headline">SK LOOKIE 동덕여대 MNM 팀,<br/>캠퍼스 리필 스테이션 프로젝트 성과 달성</h2>
+                    <p className="article-date">2024.11.07</p>
                     <p className="article-description">
-                        동덕여대 재학생들의 인-캠퍼스 리필스테이션 프로젝트<br/> Cam;coder Project가 성황리에 마무리되었다.<br/><br/>
-                        해당 프로젝트에는 동덕여대, 서울여대, 세종대 등이 참여하며<br/> 서울권 대학 내 에코 소비 문화를 확산시켰다는 평을 얻고 있다.
+                        SK LOOKIE의 동덕여대 소속 MNM팀 <br/> 뽑기 기계를 활용한 캠퍼스 맞춤형 리필 스테이션 프로젝트 혁신적 성과 달성<br/><br/>
+                        ‘LESS BUT BETTER’은 우리나라의 재활용률의 민낯을 비추며,<br/> 낮은 비율의 재활용에 의존하기보다는 사용량을 줄이는 선제적인 대처가 필요하다는 취지이다.
                     </p>
-                    <button className="article-button">기사 바로가기</button>
+                    <button
+                        className="article-button"
+                        onClick={() => window.location.href = 'https://www.nbntv.co.kr/news/articleView.html?idxno=3027691'}
+                    >
+                        기사 바로가기
+                    </button>
                 </div>
             </div>
         </div>
